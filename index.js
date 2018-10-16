f28de4f1e08d608eaf08d1c684d5e0c1f67cd4f9
7RedNights <40869403+7RedNights@users.noreply.github.com>
2018-10-16 15:22:49 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client();
+
+//Command #1
+bot.on('message', (message)=>{
+    if(message.content == "*Events"){
+        message.channel.sendMessage('https://www.instagram.com/p/BoiFHScHGJ0is4gSZkSxXdeDYzy8VyXqhy7LTU0/?taken-by=honk4lcse');
+    }
+});
+//Command #2
+{
+bot.on('message', (message)=>{
+    if(message.content == "*Help"){
+        message.channel.sendMessage('Coming Soon');
+    }
+});
+};
+//Command #3
+{
+    bot.on('message',(message)=>{
+        if(message.content== "*News"){
+            message.channel.sendMessage('https://twitter.com/NPSchools/status/1051517390155075585')
+        }
+    });
+};
+//command #4
+{
+    bot.on('message',(message)=>{
+        if(message.content== "*"){
+            message.channel.sendMessage()
+        }
+    })
+};
+
+bot.on('ready', function(){
+    console.log("Ready");
+});
+
+bot.login('NTAwMzIwMzk1NjY5NDA1NzE2.DqUWxA.K3Cr0nbDaMSV0Rl9MzfNV_02fCg');
